6bb1781aeca13dbd11cddc723fc113d1bb85e1a5
Juan Villegas <juancarlos.villegas@gmail.com>
2023-09-10 16:56:33 -0600

    Add containers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 const App = () => {
-  return (
-
-      <div className="app">
-        <section className="side-bar">
-          <button>+ New chat</button>
-          <ul className="history"></ul>
-          <nav>
-            <p>Made by Juan</p>
-          </nav>
-        </section>
-        <section className="main"></section>
-      </div>
-
-  );
+    return (
+        <div className="app">
+            <section className="side-bar">
+                <button>+ New chat</button>
+                <ul className="history">
+                    <li>blah blah blah</li>
+                </ul>
+                <nav>
+                    <p>Made by Juan</p>
+                </nav>
+            </section>
+            <section className="main">
+                <h1>JuanGPT</h1>
+                <ul className="history">
+                </ul>
+                <div className="bottom-section">
+                    <div className="input-container">
+                        <input/>
+                        <div id="submit">➢</div>
+                        <p className="info">
+                            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts
+                        </p>
+                    </div>
+                </div>
+             </section>
+        </div>
+    );
 }
 
 export default App;
